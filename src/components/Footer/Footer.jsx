cebd759572f5d768afb9a6b8a2d00f359a9c13fa
sarathchandt <sarathchand770@gmail.com>
2023-03-06 20:26:03 +0530

    again footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,23 +12,19 @@ function Footer() {
 
     return (
         <>
-               <div className={'  bg-darkGreen flex mt-10 justify-center'}>
+            <div className='  bg-darkGreen flex mt-10 justify-center'>
                 <div className='container  '>
                     <div className="row">
                         <div className="col-12">
-                            <div className='bg-darkGreen  w-full footer hidden md:block '>
-
-                                <div className='flex mt-4  justify-center '>
-                                    <p className='flex green  font-bold ' >Make</p>
-                                    <p className='red  font-bold ' >frames</p>
-                                </div>
-                                <div className="col-12 d-flex justify-content-center">
-                                    <p className='text-green'>&copy;All rights holding by makeframes</p>
-                                </div>
+                            <div className='flex mt-4  justify-center '>
+                                <p className='flex green  font-bold ' >Make</p>
+                                <p className='red  font-bold ' >frames</p>
                             </div>
                         </div>
+                        <div className="col-12 d-flex justify-content-center mb-3">
+                            <p className='text-green'>&copy;All rights holding by makeframes</p>
+                        </div>
                     </div>
-
                 </div>
             </div>
         </>
