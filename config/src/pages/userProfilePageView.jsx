fb0d0ef49a4e4ba912bbe3ffa888b41fe114ed4d
sarathchandt <sarathchand770@gmail.com>
2023-03-07 19:11:17 +0530

    capital

--- config/src/pages/userProfilePageView.jsx
+++ config/src/pages/userProfilePageView.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/Home/Header'
-import UserProfile from  '../components/userProfilePage/userProfilePage'
+import UserProfile from  '../components/userProfilePage/UserProfilePage'
 import Footer from '../components/Footer/Footer'
 
 function userProfilePageView() {
