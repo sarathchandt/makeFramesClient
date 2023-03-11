52b241e82c741f23de6784a14a5b238db0ee53b6
sarathchandt <sarathchand770@gmail.com>
2023-03-11 16:20:15 +0530

    removed config

--- config/src/components/Footer/adminFooter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function adminFooter() {
-  return (
-    <div>adminFooter</div>
-  )
-}
-
-export default adminFooter
--- config/src/components/chatBox/chatUsers.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function chatUsers() {
-  return ( 
-    <><div>chatUuuahdashdjsers</div><p></p></>
-  )
-}
-
-export default chatUsers
