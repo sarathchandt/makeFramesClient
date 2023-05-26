68802fffd12648be8c26121d62ddd63118eb4e83
sarathchandt <sarathchand770@gmail.com>
2023-05-26 12:21:36 +0530

    changed url

--- API/apiCall.js
+++ API/apiCall.js
@@ -1,7 +1,9 @@
 
 
 
-export const UURL = 'https://makeframes.herewego.shop/user/'
-export const UARL = 'https://makeframes.herewego.shop/admin/'
-// export const UURL = 'http://localhost:3033/user/'
+export const UURL = 'https://makeframes.onrender.com/user'
+export const UARL = 'https://makeframes.onrender.com/admin'
+// export const UURL = 'https://makeframes.herewego.shop/user/'
+// export const UARL = 'https://makeframes.herewego.shop/admin/'
+// // export const UURL = 'http://localhost:3033/user/'
 // export const UARL = 'http://localhost:3033/admin/'
